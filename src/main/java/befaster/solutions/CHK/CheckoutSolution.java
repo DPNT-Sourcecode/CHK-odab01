8ed5bc9518e73c1e1c515653f794f7115ad98179
sbx_user1051 <sbx_user1051@169.254.71.133>
2024-02-26 14:57:02 +0000

    Mon Feb 26 14:57:02 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -137,12 +137,13 @@ public class CheckoutSolution {
     private boolean isOfferApplicable(Map<Character, Integer> basket, Offer offer) {
         if (basket.containsKey(offer.item) && basket.get(offer.item) >= offer.units) {
             if (offer.freeItem != null) {
-                return true;
+                return basket.containsKey(offer.freeItem);
             } else {
                 // The offer is only worth it in case the free item is already in the basket
-                return basket.containsKey(offer.freeItem);
+                return true;
             }
         }
         return false;
     }
 }
+

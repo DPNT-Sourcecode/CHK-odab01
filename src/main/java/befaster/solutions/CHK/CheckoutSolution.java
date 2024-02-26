6f810c5a4fefb4603902af0d4513dcdc81fac8ae
sbx_user1051 <sbx_user1051@169.254.71.133>
2024-02-26 11:35:35 +0000

    Mon Feb 26 11:35:35 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,8 +1,17 @@
 package befaster.solutions.CHK;
 
+import befaster.supermarket.StockKeepingUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckoutSolution {
+
+    List<StockKeepingUnit> skus = List.of(new StockKeepingUnit("", 50d));
+
     public Integer checkout(String skus) {
         System.out.println(skus);
         return -1;
     }
 }
+
--- src/main/java/befaster/supermarket/StockKeepingUnit.java
+++ src/main/java/befaster/supermarket/StockKeepingUnit.java
@@ -3,6 +3,14 @@ package befaster.supermarket;
 public class StockKeepingUnit {
 
     private String item;
-    private double 
+    private double price;
+
+    public StockKeepingUnit constructor(String item, double price) {
+        StockKeepingUnit sku = new StockKeepingUnit();
+        sku.item = item;
+        sku.price = price;
+        return sku;
+    }
 
 }
+
